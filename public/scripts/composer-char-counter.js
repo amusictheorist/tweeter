743977130d8f2c1f534fc7fc5d1d099caa66b2c7
amusictheorist <a_millaresthomson@outlook.com>
2024-10-09 09:12:24 -0400

    Add tweet container and tweet.css to style it

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
-  $("#tweet-text").on("input", function() {
+  $("#tweet-text").on("keyup", function() {
     const text = $(this).val();
     const length = text.length;
     const remaining = 140 - length;
-    const counter = $(this).siblings().children().eq(1);
+    const counter = $(this).closest("form").find("output");
+    
     $(counter).text(remaining);
     if (remaining < 0) {
       $(counter).addClass("warning");
